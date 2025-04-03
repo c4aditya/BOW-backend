83fa566bf23e9c5edb3626b9d9ceaa7a8d7f1275
Aditya Singh <singhas1418@gmail.com>
2025-04-03 22:49:17 +0530

    faltu

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 
 app.use(cors({
-    origin:    ['https://www.botwaviation.com', 'http://localhost:4000'], // Allow only this domain to access the API
+    origin:    ['hhtps://api.botwaviation.com ', 'http://localhost:4000'], // Allow only this domain to access the API
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true,// Allowed headers
