1266520ce35ebfbdc3a4215a1ed42b64f24e1d2c
Aditya singh <singhas1418@gmail.com>
2025-07-09 13:11:58 +0200

    adding the controller files

--- routes/onlineAddmission.js
+++ routes/onlineAddmission.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 const onlineAddmissionDetails = require("../controllers/onlineAddmission");
 
-router.post("/onlineAddmissionDetails",  onlineAddmissionDetails);
-
-
+router.post(
+  "/onlineAddmissionDetails",
+  upload.fields([
+    { name: 'tenthFile', maxCount: 1 },
+    { name: 'twelfthFile', maxCount: 1 },
+    { name: 'graduationFile', maxCount: 1 },
+    { name: 'postGraduationFile', maxCount: 1 }
+  ]),
+  onlineAddmissionDetails
+);
 
 module.exports = router;
+
