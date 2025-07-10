2cde16b6de03aafb08f2f8a612258c9214d01500
Aditya Singh <singhas1418@gmail.com>
2025-07-10 11:42:58 +0530

    modify the model in online addmission

--- model/onlneaddmission.js
+++ model/onlneaddmission.js
@@ -3,38 +3,38 @@ const mongoose = require("mongoose");
 const admissionSchema = new mongoose.Schema({
     firstName: {
         type: String,
-        // required: true,
-        // maxlength: 50
+        required: true,
+        maxlength: 50
     },
     lastName: {
         type: String,
-        // required: true,
-        // maxlength: 50
+        required: true,
+        maxlength: 50
     },
     fatherName: {
         type: String,
-        // required: true,
-        // maxlength: 50
+        required: true,
+        maxlength: 50
     },
     email: {
         type: String,
-        // required: true,
+        required: true,
         unique: false,
         maxlength: 100
     },
     mobno: {
         type: String,
-        // required: true,
+        required: true,
         maxlength: 15
      },
     gender: {
         type: String,
-        // required: true,
+        required: true,
         enum: ["Male", "Female", "Other"]
     },
     course: {
         type: String,
-        // required: true
+        required: true
     },
 
     // ✅ Add file path fields here:
